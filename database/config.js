7768b1a3f25ceb4f84b9bb89f6bf1cd20b9910f9
Centauride <centauride.dev@gmail.com>
2022-06-03 19:58:13 +0200

    mongoose connect param added again

--- database/config.js
+++ database/config.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
     try {
         console.log('init db config');
-        await mongoose.connect(process.env.DB_CNN);
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser : true,
+            useUnifiedTopology : true,
+            // useCreateIndex : true,
+        });
         console.log('DB Online');
 
     } catch (error) {
